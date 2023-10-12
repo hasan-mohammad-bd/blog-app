5547f449079ed942f2f72639f7750ac79269b4d1
Hasan Mohammad <hasan.md.east@gmail.com>
2023-10-12 16:04:25 +0600

    adding footer

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import styles from "./menu.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
+import MenuPost from '../menuPost/MenuPost';
+import MenuCategory from '../menuCategory/menuCategory';
 const Menu = () => {
     return (
         <div className={styles.container}>
-            Menu
+
+            {/* Most Popular */}
+             <h2 className={styles.subtitle}>{"Thats hot"}</h2>
+           <h1 className={styles.title}>Most Popular</h1>
+            <MenuPost withImage={false}/>
+           {/* catagories */}
+           <h2 className={styles.subtitle}>Discover by the </h2>
+           <h1 className={styles.title}>Categories</h1>
+           <MenuCategory/>
+
+
+           {/* Chosen by the Editor Section */}
+           <h2 className={styles.subtitle}>{"Chosen by the editor"}</h2>
+           <h1 className={styles.title}>Editors Pick</h1>
+            <MenuPost withImage={true}/>
+
         </div>
     );
 };
