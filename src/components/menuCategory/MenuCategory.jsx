5a72ed8e97db2ea29dbeb6ab18e9da9025d527f9
Hasan Mohammad <hasan.md.east@gmail.com>
2023-10-24 10:02:06 +0600

    adding categorty

---
+++ src/components/menuCategory/MenuCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MenuCategory = () => {
+    return (
+        <div>
+            this is a MenuCategory file
+        </div>
+    );
+};
+
+export default MenuCategory;
