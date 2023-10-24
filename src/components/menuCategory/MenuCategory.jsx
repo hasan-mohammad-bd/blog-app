63e79ad56859c9a2f6b887480f2214410b48e36a
Hasan Mohammad <hasan.md.east@gmail.com>
2023-10-24 10:08:47 +0600

    adding menuCategory

--- src/components/menuCategory/MenuCategory.jsx
+++ src/components/menuCategory/MenuCategory.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-
-const MenuCategory = () => {
-    return (
-        <div>
-            this is a MenuCategory file
-        </div>
-=======
 import Link from 'next/link';
 import React from 'react';
 import styles from '../Menu/menu.module.css'
@@ -20,7 +11,6 @@ const MenuCategory = () => {
         <Link href="/blog?cat=food" className={`${styles.category} ${styles.food}`}>food</Link>
         <Link href="/blog?cat=code" className={`${styles.category} ${styles.coding}`}>Code</Link>
        </div>
->>>>>>> 018d31409035ffb0d5b5f194b26db5e7e1fef9e2
     );
 };
 
